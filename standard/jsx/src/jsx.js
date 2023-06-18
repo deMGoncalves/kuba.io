e0c494bf9e3d8b30b5a23657d725385c2699215b
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-18 18:47:11 -0300

    docs(jsx): documentando o codigo jsx

--- standard/jsx/src/jsx.js
+++ standard/jsx/src/jsx.js
@@ -2,14 +2,26 @@ import Children from './children'
 import Componet from './component'
 import Element from './element'
 
+/**
+ * Renderiza um elemento JSX com base no elemento ou componente fornecido, atributos e filhos.
+ *
+ * @param {string | Function} elementOrComponent - Elemento JSX ou componente a ser renderizado.
+ * @param {Object} attrs - Atributos para o elemento JSX ou componente.
+ * @param {Array<string | Function>} children - Filhos do elemento JSX ou componente. Pode conter strings ou elementos JSX.
+ * @returns {string} O elemento JSX renderizado como uma string.
+ */
 function jsx (elementOrComponent, attrs, ...children) {
+  // Copia os atributos para evitar modificações indesejadas no objeto original
   attrs = { ...attrs }
+
+  // Mapeia os filhos para garantir que todos estejam no formato adequado
   children = Children.mapper(children)
 
-  if (Componet.is(elementOrComponent))
-    return Componet.execute(elementOrComponent, attrs, children)
-
-  return Element.create(elementOrComponent, attrs, children)
+  // Se o elemento for um componente, executa o componente com os atributos e filhos fornecidos
+  // Caso contrário, cria um novo elemento JSX com os atributos e filhos fornecidos
+  return Componet.is(elementOrComponent)
+    ? Componet.execute(elementOrComponent, attrs, children)
+    : Element.create(elementOrComponent, attrs, children)
 }
 
 export default jsx
--- standard/router/src/handle.js
+++ standard/router/src/handle.js
@@ -2,7 +2,7 @@ import listeners from './listeners'
 
 function handle (request, env, ctx) {
   const url = new URL(request.url)
-  const { page } = listeners[request.method][0]
+  const { page } = listeners[request.method].find(({ path }) => (path === url.pathname))
   return page(request, env, ctx)
 }
 
