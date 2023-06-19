7ad3bd2edd1112badc51a12150fe074c5163d2aa
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-19 08:42:02 -0300

    docs(jsx.props): explicacao base da unidade construtiva

--- standard/jsx/src/props.js
+++ standard/jsx/src/props.js
@@ -1,7 +1,16 @@
+/**
+ * Utility class for handling props (attributes) of JSX elements.
+ */
 class Props {
-  static stringfy (attrs) {
-    return Object
-      .entries(attrs)
+  /**
+   * Converts an object of props (attributes) into a string representation.
+   *
+   * @static
+   * @param {Object} attrs - The props (attributes) object.
+   * @returns {string} The string representation of the props.
+   */
+  static stringfy(attrs) {
+    return Object.entries(attrs)
       .map(([key, value]) => `${key}='${value}'`)
       .join(' ')
   }
