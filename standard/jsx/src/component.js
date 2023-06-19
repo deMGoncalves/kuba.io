aaefccf049b6e220ed291d5c47a86ce7af270e1c
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-19 08:28:54 -0300

    docs(jsx.component): documentacao base

--- standard/jsx/src/component.js
+++ standard/jsx/src/component.js
@@ -1,10 +1,29 @@
+/**
+ * Utility class for handling components in JSX.
+ */
 class Componet {
-  static execute (component, attrs, children) {
+  /**
+   * Executes the given component with the provided attributes and children.
+   *
+   * @static
+   * @param {Function} component - The component function to be executed.
+   * @param {Object} attrs - The attributes to be passed to the component.
+   * @param {Array} children - The children to be passed to the component.
+   * @returns {*} The result of executing the component.
+   */
+  static execute(component, attrs, children) {
     return component(attrs, children)
   }
 
-  static is (component) {
-    return (typeof component === 'function')
+  /**
+   * Checks if the given value is a valid component.
+   *
+   * @static
+   * @param {*} component - The value to be checked.
+   * @returns {boolean} True if the value is a component, false otherwise.
+   */
+  static is(component) {
+    return typeof component === 'function'
   }
 }
 
