6b2e09a8929714159fc0fb67b2d8d97c2bd839c0
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-19 08:43:49 -0300

    docs(jsx.tag): documentacao base da class

--- standard/jsx/src/tag.js
+++ standard/jsx/src/tag.js
@@ -1,6 +1,16 @@
+/**
+ * Utility class for working with tags in JSX elements.
+ */
 class Tag {
-  static isSelfClose (children) {
-    return (children?.length == 0)
+  /**
+   * Checks if a tag is a self-closing tag based on the presence of children.
+   *
+   * @static
+   * @param {Array} children - The children of the tag.
+   * @returns {boolean} True if the tag is self-closing, false otherwise.
+   */
+  static isSelfClose(children) {
+    return (children?.length === 0)
   }
 }
 
