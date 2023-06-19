f1057536ff02f21b16886987040ed6999ec01155
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-19 08:26:29 -0300

    docs(jsx.children): documentacao base da class

--- standard/jsx/src/children.js
+++ standard/jsx/src/children.js
@@ -1,14 +1,29 @@
 import Instance from './instance'
 
+/**
+ * Utility class for handling JSX children.
+ */
 class Children {
-  static mapper (children) {
+  /**
+   * Maps and filters the JSX children to ensure they are in the proper format.
+   *
+   * @param {Array} children - The JSX children to be mapped and filtered.
+   * @returns {Array} The mapped and filtered JSX children.
+   */
+  static mapper(children) {
     children = children.flat(Infinity)
     children = children.filter(Boolean)
     children = Instance.mapper(children)
     return children
   }
 
-  static stringfy (children) {
+  /**
+   * Converts the JSX children into a string.
+   *
+   * @param {Array} children - The JSX children to be converted into a string.
+   * @returns {string} The string representation of the JSX children.
+   */
+  static stringify(children) {
     return children.join('')
   }
 }
