c84d2bca22505d6e40bb3a554607c487e1c914b9
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-21 08:26:04 -0300

    docs(middleware): documentacao base do pacote

--- standard/middleware/src/middleware.js
+++ standard/middleware/src/middleware.js
@@ -1,15 +1,43 @@
-function middleware (functionRef) {
+/**
+ * Creates a middleware that wraps a class constructor and adds additional functionality.
+ *
+ * @param {Function} functionRef - The middleware function to be executed.
+ * @returns {Function} The wrapped class constructor.
+ */
+function middleware(functionRef) {
   return function (ClassRef) {
     return new Proxy(
+      /**
+       * Creates a new instance of the class and executes the middleware function.
+       *
+       * @returns {Promise} A promise that resolves to the instance of the class.
+       */
       function () {
         return new Promise(async function (resolve) {
           const instance = await new ClassRef(...arguments)
-          functionRef(instance, () => resolve(instance))
+          const next = () => resolve(instance)
+          functionRef(instance, next)
         })
       },
       {
-        get: (_target, key) => Reflect.get(ClassRef, key),
-        set: (_target, key, value) => (Reflect.set(ClassRef, key, value), true)
+        /**
+         * Retrieves the value of a property from the class constructor.
+         *
+         * @param {Object} _ - The target object.
+         * @param {string | symbol} key - The property key.
+         * @returns {*} The value of the property.
+         */
+        get: (_, key) => Reflect.get(ClassRef, key),
+
+        /**
+         * Sets the value of a property in the class constructor.
+         *
+         * @param {Object} _ - The target object.
+         * @param {string | symbol} key - The property key.
+         * @param {*} value - The value to be set.
+         * @returns {boolean} A boolean indicating if the assignment was successful.
+         */
+        set: (_, key, value) => (Reflect.set(ClassRef, key, value), true)
       }
     )
   }
