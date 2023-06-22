ccec7a4d37b78f01203b9f6475eb735ae2fbb9f8
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-22 09:26:45 -0300

    docs(router): documentando a implementacao do pacote

--- standard/router/src/extractArgs.js
+++ standard/router/src/extractArgs.js
@@ -1,5 +1,12 @@
 import args from './args'
 
+/**
+ * Extract Request Arguments
+ *
+ * Extracts query string parameters from the request URL and stores them in the args object.
+ *
+ * @param {Request} request - The request object containing the URL with query string parameters.
+ */
 function extractArgs (request) {
   const url = new URL(request.url)
   const search = new URLSearchParams(url.search)
--- standard/router/src/extractParams.js
+++ standard/router/src/extractParams.js
@@ -1,5 +1,13 @@
 import params from './params'
 
+/**
+ * Extract Path Parameters
+ *
+ * Extracts path parameters from the request URL and stores them in the params object.
+ *
+ * @param {Request} request - The request object containing the URL.
+ * @param {string} path - The path pattern with placeholders for parameters (e.g., '/users/:id').
+ */
 function extractParams (request, path) {
   const url = new URL(request.url)
   const keys = path?.split('/')
