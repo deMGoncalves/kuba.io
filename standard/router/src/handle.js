f5c4989d8b790fd21f77d87a4681a52bf4f6a9e5
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-22 08:04:06 -0300

    chore(router.handle): logica para montar o validador de url

--- standard/router/src/handle.js
+++ standard/router/src/handle.js
@@ -1,9 +1,11 @@
 import listeners from './listeners'
 
 function handle (request, env, ctx) {
-  const url = new URL(request.url)
-  const { page } = listeners[request.method].find(({ path }) => (path === url.pathname))
-  return page(request, env, ctx)
+  const { pathname } = new URL(request.url)
+  const path = pathname.replace(/:\w+/g, '([a-z0-9-_]+)')
+  const pattern = new RegExp(`^${path}$`, 'i')
+  const { page } = listeners[request.method].find(({ path }) => pattern.test(path)) ?? {}
+  return page?.(request, env, ctx)
 }
 
 export default handle
