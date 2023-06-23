499def4799b913b8113a776c29edf42ccfcc758c
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-23 13:02:35 -0300

    docs(css.classselectorregex): doc base para a unidade

--- standard/css/src/classSelectorRegex.js
+++ standard/css/src/classSelectorRegex.js
@@ -1,3 +1,7 @@
+/**
+ * Expressão regular para selecionar classes em um seletor CSS.
+ * @type {RegExp}
+ */
 const classSelectorRegex = /\.[a-zA-Z0-9_-]+(?![^()]*\))/g
 
 export default classSelectorRegex
