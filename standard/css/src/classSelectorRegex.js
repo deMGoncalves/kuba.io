adea0e5aabf6de2383d9d5fb47644a9808552d40
deMGoncalves <cleber.engineer@hotmail.com>
2023-06-27 09:11:38 -0300

    fix(css): preciso ignorar os numerors

--- standard/css/src/classSelectorRegex.js
+++ standard/css/src/classSelectorRegex.js
@@ -3,6 +3,6 @@
  *
  * @type {RegExp}
  */
-const classSelectorRegex = /\.[a-zA-Z0-9_-]+(?![^()]*\))/g
+const classSelectorRegex = /\.[a-z_][a-z0-9_-]*(?![^()]*\))/ig
 
 export default classSelectorRegex
