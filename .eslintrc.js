de0d9fc1bfda97c8affc1c69326195989e79dbf0
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-01 22:52:29 -0300

    ci: configuracao do eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    'browser': true,
+    'es2021': true
+  },
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^(h|Fragment)$',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}
