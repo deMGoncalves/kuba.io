7c27569a14e061307d60860f338494dc2366b266
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-01 23:25:37 -0300

    ci: configuracao do estylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    'browser': true,
-    'es2021': true
+    browser: true,
+    es2021: true
   },
   extends: [
     'standard',
@@ -10,8 +10,8 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   plugins: [
     '@typescript-eslint'
