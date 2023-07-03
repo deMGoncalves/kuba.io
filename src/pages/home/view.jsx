9847ebbdfcbd347915ba4c1539c8fb1354938c65
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-03 17:22:18 -0300

    refactor(home): usando o texto para escrever texto

--- src/pages/home/view.jsx
+++ src/pages/home/view.jsx
@@ -1,11 +1,12 @@
 import h from '@standard/h'
 import Master from '@standard/master'
+import text from '@elements/text'
 
 function component (address) {
   return (
     <Master>
       <address>
-        <p>{address.value}</p>
+        <text.P>{address.value}</text.P>
       </address>
     </Master>
   )
