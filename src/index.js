6993126b40ddb064bd264ad3fc683586af7154ac
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-04 13:08:50 -0300

    docs: explicando o fetch do worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import '@pixel/tokens'
 import '@pages/home'
 import router from '@standard/router'
 
+/**
+ * The main export object for the application.
+ * @namespace
+ * @property {function} fetch - A function to handle routing requests.
+ * @default router.handle
+ */
 export default {
   fetch: router.handle
 }
