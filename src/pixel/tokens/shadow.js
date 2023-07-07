f4ed3969425346f0b17d0b9d04fc8e99091f0b76
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-07 10:37:40 -0300

    refactor(shadow): usando as referencias do m3

--- src/pixel/tokens/shadow.js
+++ src/pixel/tokens/shadow.js
@@ -5,10 +5,12 @@ import css from '@standard/css'
  */
 const style = css`
   :root {
-    --shadow-level-1: 0 4px 8px;
-    --shadow-level-2: 0 8px 24px;
-    --shadow-level-3: 0 16px 32px;
-    --shadow-level-4: 0 16px 48px;
+    --shadow-level-0: none;
+    --shadow-level-1: 0 1px 1px 0 rgb(0 0 0 / 14%), 0 2px 1px -1px rgb(0 0 0 / 12%), 0 1px 3px 0 rgb(0 0 0 / 20%);
+    --shadow-level-2: 0 3px 4px 0 rgb(0 0 0 / 14%), 0 3px 3px -2px rgb(0 0 0 / 12%), 0 1px 8px 0 rgb(0 0 0 / 20%);
+    --shadow-level-3: 0 6px 10px 0 rgb(0 0 0 / 14%), 0 1px 18px 0 rgb(0 0 0 / 12%), 0 3px 5px -1px rgb(0 0 0 / 20%);
+    --shadow-level-4: 0 8px 10px 1px rgb(0 0 0 / 14%), 0 3px 14px 2px rgb(0 0 0 / 12%), 0 5px 5px -3px rgb(0 0 0 / 20%);
+    --shadow-level-5: 0 12px 17px 2px rgb(0 0 0 / 14%), 0 5px 22px 4px rgb(0 0 0 / 12%), 0 7px 8px -4px rgb(0 0 0 / 20%);
   }
 `
 
